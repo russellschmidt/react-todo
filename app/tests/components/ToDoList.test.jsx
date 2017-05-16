215800f48968a9bacc743314ddbe0ca335faa06c
Russell Schmidt <russell.schmidt@me.com>
2017-05-15 17:02:07 -0700

    add test for array of todolist items

--- app/tests/components/ToDoList.test.jsx
+++ app/tests/components/ToDoList.test.jsx
@@ -5,9 +5,19 @@ var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
 var ToDoList = require('ToDoList');
+var ToDo = require('ToDo');
 
 describe('ToDoList component', () => {
   it('should exist', () => {
     expect(ToDoList).toExist();
   });
+
+  it('should render 1 todo component for each todo item', () => {
+    var todos = [ {id:1, text: 'do something'}, {id: 2, text: 'do dat'}];
+
+    var todoList = TestUtils.renderIntoDocument(<ToDoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, ToDo);
+
+    expect(todosComponents.length).toBe(todos.length);
+  })
 });
