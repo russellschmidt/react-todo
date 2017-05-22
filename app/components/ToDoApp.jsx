6db4153e43949fd798b4e2650814c5f5d4260cd9
Russell Schmidt <russell.schmidt@me.com>
2017-05-22 16:59:50 -0700

    add ability to add new to-dos to state

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ToDoList = require('ToDoList');
 var AddToDo = require('AddToDo');
 var ToDoSearch = require('ToDoSearch');
+var uuid = require('node-uuid');
 
 var ToDoApp = React.createClass({
   getInitialState : function () {
@@ -10,26 +11,34 @@ var ToDoApp = React.createClass({
       searchText: '',
       todos: [
         {
-          id: 1,
+          id: uuid(),
           text: 'Walk the dog'
         },
         {
-          id: 2,
+          id: uuid(),
           text: 'Clean the floor'
         },
         {
-          id: 3,
+          id: uuid(),
           text: 'Smack my children'
         },
         {
-          id: 4,
+          id: uuid(),
           text: 'Slap my camel'
         }
       ]
     }
   },
   handleAddToDo: function (text) {
-    alert("New To Do: " + text);
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id:   uuid(),
+          text: text
+        }
+      ]
+    })
   },
   handleSearch: function(showCompleted, searchText) {
     this.setState({
--- app/tests/components/ToDoApp.test.jsx
+++ app/tests/components/ToDoApp.test.jsx
@@ -4,10 +4,20 @@ var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
-var ToDoApp = require('ToDoList');
+var ToDoApp = require('ToDoApp');
 
-describe('ToDoApp component', () => {
+describe('ToDoApp', () => {
   it('should exist', () => {
     expect(ToDoApp).toExist();
+  });
+
+  it('should add todo to the todos state on handleAddTodo', () => {
+    var todoText = "this is a to do item test test 123";
+    var todoApp = TestUtils.renderIntoDocument(<ToDoApp/>);
+
+    todoApp.setState({todos: []}); //empty the array
+    todoApp.handleAddToDo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
   });
 });
