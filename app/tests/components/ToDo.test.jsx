f8bd8c0bb200fd91132c09ada0b79677206dc66d
Russell Schmidt <russell.schmidt@me.com>
2017-05-23 11:04:52 -0700

    add test to ToDo.test and ToDoList.test

--- app/tests/components/ToDo.test.jsx
+++ app/tests/components/ToDo.test.jsx
@@ -10,4 +10,18 @@ describe('ToDo component', () => {
   it('should exist', () => {
     expect(ToDo).toExist();
   });
+
+  it("should call onToggle prop with id on click", () => {
+    var toDoData = {
+      id: 69,
+      text: "Write todo.test.jsx test",
+      completed: true
+    };
+    var spy = expect.createSpy();
+    var todo = TestUtils.renderIntoDocument(<ToDo {...toDoData} onToggle={spy}/>);
+    // simulate click event by selecting the root element [0] using jquery
+    var $el = $(ReactDOM.findDOMNode(todo));
+    TestUtils.Simulate.click($el[0]);
+    expect(spy).toHaveBeenCalledWith(toDoData.id);
+  });
 });
--- app/tests/components/ToDoApp.test.jsx
+++ app/tests/components/ToDoApp.test.jsx
@@ -20,4 +20,19 @@ describe('ToDoApp', () => {
 
     expect(todoApp.state.todos[0].text).toBe(todoText);
   });
+
+  it('should toggle *completed value when handleToggle is called', () => {
+    var toDoData = {
+      id: 11,
+      text: 'test features',
+      completed: false
+    };
+
+    var toDoApp = TestUtils.renderIntoDocument(<ToDoApp/>);
+    toDoApp.setState({todos: [toDoData]});
+
+    expect(toDoApp.state.todos[0].completed).toBe(false);
+    toDoApp.handleToggle(toDoData.id);
+    expect(toDoApp.state.todos[0].completed).toBe(true); 
+  });
 });
