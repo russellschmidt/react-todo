b1a31ab70f49df98f21b88513007148b38302947
Russell Schmidt <russell.schmidt@me.com>
2017-06-02 12:35:17 -0700

    add todos reducer and test for toggle todo and add todo

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,38 @@ describe('Reducers', () => {
     });
   });
 
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [{
+        text: 'Walk the cat',
+        id: 123,
+        completed: true,
+        createdAt: 111111,
+        completedAt: 555555
+      }];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 123
+      };
+
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+  });
+
 });
