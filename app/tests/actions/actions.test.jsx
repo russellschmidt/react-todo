92b6422f3c09ae2e6f523597e8cfebd59b7445fe
Russell Schmidt <russell.schmidt@me.com>
2017-06-01 17:08:13 -0700

    got actions and tests done for to do move to redux

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,48 @@
+var expect = require('expect');
+var actions = require('actions');
+
+describe('Actions', () => {
+  it('should generate search text action', () => {
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'Some text'
+    };
+
+    var res = actions.setSearchText(action.searchText);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate add todo action', () => {
+    var action = {
+      type: 'ADD_UNDERSCORE_TODO',
+      text: 'To do item'
+    };
+
+    var res = actions.addTodo(action.text);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate toggleShowCompleted action', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+
+    var res = actions.toggleShowCompleted();
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate action id', () => {
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: 1
+    };
+
+    var res = actions.toggleTodo(action.id);
+
+    expect(res).toEqual(action);
+  });
+
+})
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ module.exports = {
     ],
     alias: {
       ApplicationStyles: 'app/styles/app.scss',
+      actions: 'app/actions/actions.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
