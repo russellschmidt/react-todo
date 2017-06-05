451d0240875ea9256dfc890c60053fa62ba333b8
Russell Schmidt <russell.schmidt@me.com>
2017-06-05 13:51:57 -0700

    delete default to do items from app.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -12,9 +12,6 @@ store.subscribe(() => {
   console.log('New state', store.getState());
 });
 
-store.dispatch(actions.addToDo('Clean the yard'));
-store.dispatch(actions.setSearchText('yard'));
-store.dispatch(actions.toggleShowCompleted());
 // // load foundation
 // require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
